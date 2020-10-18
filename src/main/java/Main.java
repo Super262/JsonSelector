28ed1977408783387c5986fec1d971429205c981
Super262 <virusformail@gmail.com>
2020-10-18 10:20:17 +0800

    Finished.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         byte[] encoded = Files.readAllBytes(Paths.get("/Users/fengwei/IdeaProjects/JsonSelector/example.json"));
         String content = new String(encoded, StandardCharsets.UTF_8);
-
         System.out.println("Path: *(Object) -> *(Object) -> *(Object, TARGET) -> *(Object) -> *(Null)");
         Selector selector = new Selector(content).addObjectSign().addObjectSign().addObjectSign().setPrevSignAsTarget().addObjectSign().addNullSign();
         for(String s : selector.getSelectedJsonString()){
